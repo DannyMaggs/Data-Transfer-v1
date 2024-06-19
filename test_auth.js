f8e7117041557e2cf714f83385029c8a681badfe
DannyMaggs <daniel.maggs@motohaus.com>
2024-06-19 11:01:07 +0100

    Added test auth.js

---
+++ test_auth.js
@@ -0,0 +1,26 @@
+const { ConfidentialClientApplication } = require('@azure/msal-node');
+
+const config = {
+    auth: {
+        clientId: '3acd75e1-dbf0-4df0-88aa-2c7a4bd5ee8b',
+        authority: 'https://login.microsoftonline.com/7f65e0c2-5159-471c-9af9-e57501d53752',
+        clientSecret: 'MlC8Q~XZ_vLrsVb4E_afMEwZVKjQBk41PjIhObS0',
+    }
+};
+
+const cca = new ConfidentialClientApplication(config);
+
+const authParams = {
+    scopes: ['https://graph.microsoft.com/.default'],
+};
+
+async function getToken() {
+    try {
+        const authResult = await cca.acquireTokenByClientCredential(authParams);
+        console.log('Access token:', authResult.accessToken);
+    } catch (error) {
+        console.error('Error acquiring token:', error);
+    }
+}
+
+getToken();
