0e134c5617ec1453e7b2283f300421fa204cbf76
DannyMaggs <daniel.maggs@motohaus.com>
2024-06-19 13:08:23 +0100

    Updated node modules

--- list_files.js
+++ list_files.js
@@ -1,13 +1,12 @@
 const { ConfidentialClientApplication } = require('@azure/msal-node');
-const fs = require('fs');
 const fetch = require('node-fetch');
 
 // Azure AD Config
 const config = {
     auth: {
-        clientId: "3acd75e1-dbf0-4df0-88aa-2c7a4bd5ee8b",
-        authority: "https://login.microsoftonline.com/7f65e0c2-5159-471c-9af9-e57501d53752",
-        clientSecret: "MlC8Q~XZ_vLrsVb4E_afMEwZVKjQBk41PjIhObS0"
+        clientId: "your-client-id",
+        authority: "https://login.microsoftonline.com/your-tenant-id",
+        clientSecret: "your-client-secret"
     }
 };
 
